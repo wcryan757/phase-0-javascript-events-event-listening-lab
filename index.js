da97b6a92ef4a4dcc3a84758bcb7b424af435ba3
William Ryan <wcryan757@gmail.com>
2021-04-02 21:45:25 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+
+    const input = document.getElementById('input');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
+
 }
+
+const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert); 
